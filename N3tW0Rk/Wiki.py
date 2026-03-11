a7e0d1596b7d69f51c307cd929e6ca6b22b6e7c2
Kieu Vo <bangkieuvo@gmail.com>
2026-03-11 21:20:27 +0700

    Update Wiki.py

--- N3tW0Rk/Wiki.py
+++ N3tW0Rk/Wiki.py
@@ -1,5 +1,21 @@
-a = 100
-b = format(a,'08b')
-c = a.to_bytes(1,byteorder = 'big')
-print(c)
-
+class IpV4():
+	def __init__(self,value = []):
+		self.value = value
+	def setIpByBinary(self):
+		returns
+	def setIpByDecimal(self,str):
+		newValue = [] 
+		l = str.split(".")
+		for char in l:
+			num = int(char)
+			if (num<0 or num>255):
+				print(f"[❌ Wrong input! ({num} is not validate)]")
+				return
+			newValue.append(num)
+		self.value = newValue.copy()
+	def show(self,choice = 0):
+		if choice == 0:
+			print(self.value)
+ip = IpV4()
+ip.setIpByDecimal("125.25.-7.1")
+print(ip.value)
